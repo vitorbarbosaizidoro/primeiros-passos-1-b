1d06f8719fc541d7c0f87a9baf7e74af2eee4bf2
vitorbarbosaizidoro <vitor.barbosa.izidoro@escola.pr.gov.br>
2026-08-24 08:16:32 -0300

    <..>

---
+++ script.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector("button");
+botao.addEventListener("click",botaoClicado);
+
+function botaoClicado (){
+    console.log("fui clicado");
+    let texto = botao.querySelector("span");
+    texto.textContent++;
+}
